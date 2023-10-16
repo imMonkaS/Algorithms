f27abea6e405bbca84cbaaab63599f98a72cdcaa
MonkaS <danchik646@gmail.com>
2023-10-17 01:05:04 +0300

    final commit (i hope so)

--- AlgoStankin.cpp
+++ AlgoStankin.cpp
@@ -7,6 +7,17 @@
 
 using namespace std;
 
+int count_elements(string filename) {
+    int counter = 0;
+    ifstream file(filename);
+    while (!file.eof()) {
+        int r;
+        file >> r;
+        counter++;
+    }
+    return counter;
+}
+
 void readNumbers(int arr[], int N, string filename) {
     ifstream file(filename);
     for (int i = 0; i < N; i++) {
@@ -17,7 +28,7 @@ void readNumbers(int arr[], int N, string filename) {
 void print(int arr[], int N, string filename) {
     ofstream file(filename);
     for (int i = 0; i < N; i++) {
-        file << arr[i] << " ";
+        file << arr[i] << endl;
     }
 }
 
@@ -34,6 +45,18 @@ bool checkArray(int arr[], int N) {
         if (arr[i] > arr[i + 1]) return false;
     }
     return true;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        // Last i elements are already 
+        // in place 
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
 }
 
 void BatchersMergeSort(int arr[], int N) {
@@ -64,28 +87,42 @@ void BatchersMergeSort(int arr[], int N) {
     }
 }
 
+int compare(const void* a, const void* b)
+{
+    const int* x = (int*)a;
+    const int* y = (int*)b;
+
+    if (*x > *y)
+        return 1;
+    else if (*x < *y)
+        return -1;
+
+    return 0;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
     const int SIZE = 5000;
     int R[SIZE];
-    int N;
-    cout << "Введите количество цифр в файле:" << endl;
-    cin >> N;
     cout << "Введите название файла:" << endl;
     string filename;
     cin >> filename;
+    int N = count_elements(filename) - 1;
 
     readNumbers(R, N, filename);
 
     auto start = chrono::steady_clock::now();
+    //bubbleSort(R, N);
+    //qsort(R, N, sizeof(int), compare);
     BatchersMergeSort(R, N);
     auto end = chrono::steady_clock::now();
     long duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
     cout << endl;
 
-    print(R, N, "sortedArray.txt");
+    cout << "Сортируется массив из " << N << " чисел" << endl;
+    print(R, N, "d" + to_string(N) + "-s.txt");
     if (checkArray(R, N)) {
         cout << "Массив отсортирован" << endl;
     }
@@ -93,6 +130,7 @@ int main()
         cout << "Массив не отсортирован" << endl;
     }
     cout << "Время выполнения: " << duration / 1000.0 << " мсек" << endl;
+    cout << "Сортированный массив записан в файл: " << "d" + to_string(N) + "-s.txt" << endl;
 
     return 0;
 }
